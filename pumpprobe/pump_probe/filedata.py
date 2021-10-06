19145fdad3d6dc8d8fe42aa5ff024b2a7e3d878b
tom_glb <zixiaowang@pku.edu.cn>
2021-10-06 09:27:56 +0800

    update

--- pumpprobe/pump_probe/filedata.py
+++ pumpprobe/pump_probe/filedata.py
@@ -17,7 +17,7 @@ class FileData():
         power = file[power_from:power_to]
         power_d = file[power_to+1:power_to+2]
 
-        self.power = int(power)+int(power_d)/10
+
 
         file_name = file_info["f_path"] + file
         data = np.loadtxt(file_name)
@@ -26,10 +26,28 @@ class FileData():
         time = data[1:-1,0]
         value = data[1:-1,2]
 
+        self.power = int(power)+int(power_d)/10
         self.file_name = file
         self.time = time
         self.position = position
         self.value = value
 
-    def shift_to_zero(self):
+    def get_peak_position(self):
+        position = np.argmax(abs(self.value))
+        return self.time[position]
+
+    def set_time_position_at_peak(self, time_peak_position):
+        self.time -= time_peak_position
+
+    def set_value_to_zero(self):
+        """Subtracting the dorsal base before the signal comes
+        Args:
+            None
+        Return:
+            None
+        """
         self.value -= self.value[0]
+
+
+
+
--- pumpprobe/pump_probe/filesort.py
+++ pumpprobe/pump_probe/filesort.py
@@ -1,13 +1,19 @@
 import os
 
-def file_sort(file_info):
+def file_sort(file_info, reverse):
     f_path = file_info["f_path"]
-    reverse = file_info["reverse"]
     power_from = file_info["power_from"]
     power_to = file_info["power_to"]
     files = os.listdir(f_path)
-
+    
     files.sort(key= lambda x:int(x[power_from:power_to]))
     if reverse:
         files.reverse()
-    return files
+
+    num_files = len(files)
+    if reverse:
+        num_power_max = 0
+    else:
+        num_power_max = num_files
+    
+    return files, num_files, num_power_max
